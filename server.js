07e0f2b31028962e2d7cbe91a925a63f05c2f530
Alexandru Popa <apopa@indeed.com>
2021-03-12 06:20:33 +0900

    Add endpoint to test connection to the Internet

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import Koa from 'koa';
+import fetch from 'node-fetch';
 
 const app = new Koa();
 let server;
@@ -27,6 +28,17 @@ app.use(async (ctx, next) => {
   if (ctx.path === '/info') {
     console.log('info');
     ctx.body = 'Last updated at: Thu Mar 11 06:51:27 JST 2021';
+    return;
+  }
+
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  if (ctx.path === '/fetch') {
+    console.log('fetch');
+    const req = await fetch('http://example.com/');
+    ctx.body = await req.text();
     return;
   }
 
